d88aa1369b77829839f9968182edb4dff7d7424a
DavidBabel <DavidBabel@users.noreply.github.com>
2019-08-19 19:34:48 +0000

    Deploying to gh-pages - 19:34:48

---
+++ precache-manifest.cf58303eaba946211c3e10cf271f9783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4475fc8d39d960e6b4fa5fb2fb48cdc3",
+    "url": "/wow-classic-pve/index.html"
+  },
+  {
+    "revision": "89e302004e3bc3151171",
+    "url": "/wow-classic-pve/static/css/main.21af13c3.chunk.css"
+  },
+  {
+    "revision": "daf45a8526205ac38f7e",
+    "url": "/wow-classic-pve/static/js/2.9f6d1e17.chunk.js"
+  },
+  {
+    "revision": "89e302004e3bc3151171",
+    "url": "/wow-classic-pve/static/js/main.832bf948.chunk.js"
+  },
+  {
+    "revision": "256547b911b49e4899b1",
+    "url": "/wow-classic-pve/static/js/runtime~main.ec1b0063.js"
+  }
+]);
